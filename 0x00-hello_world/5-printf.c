4635afd1331550d1734e9c1ac46fc3c44c221b0f
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-16 16:20:15 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program to print a text using printf
+ * Return: 0 (completed)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
