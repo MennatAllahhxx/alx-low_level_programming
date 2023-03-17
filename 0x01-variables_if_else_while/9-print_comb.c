a71780c0a0342222a535342675a72eb8039eb360
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 16:59:53 -0700

    patience persistance and perspiration make an unbearable combbination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,16 @@
 int main(void)
 {
 int i = 0;
-while (i < 9)
+while (i <= 9)
 {
 putchar('0' + i);
+if (i < 9)
+{
 putchar(44);
 putchar(32);
+}
 i++;
 }
-putchar('0' + i);
+putchar('\n');
 return (0);
 }
