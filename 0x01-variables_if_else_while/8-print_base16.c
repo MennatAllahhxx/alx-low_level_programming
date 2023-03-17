1524695b95c10e18b5c46589872385d434feb0bd
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 16:30:55 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 /**
- * main - a program to print all numbers of base 10
+ * main - a program to print all numbers of base 16
  * Return: 0 (success)
  */
 int main(void)
 {
 int i = 0;
+char c = 'A';
 while (i < 10)
 {
 putchar('0' + i);
 i++;
 }
+while (c < 'E')
+{
+putchar(c);
+c++; }
 putchar('\n');
 return (0);
 }
