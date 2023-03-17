269601848f6f80425be7e28e30886bf50a8c940b
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 16:01:08 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main - a program to print all numbers of base 10
+ * return: 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+	printf("%d", i);
+printf("\n");
+return (0);
+}
