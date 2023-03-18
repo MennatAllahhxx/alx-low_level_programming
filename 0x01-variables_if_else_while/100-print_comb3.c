f203e5a0dd120379c7cf98fa8e61d73c1c67e0d2
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 18:06:32 -0700

    inverting is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program to print all possible combinations of 2 different digits
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i = 0, j;
+while (i < 10)
+{
+j = i + 1;
+while (j < 10)
+{
+if (i != j)
+{
+putchar('0' + i);
+putchar('0' + j);
+if ((i != 8) || (j != 9))
+{
+putchar(44);
+putchar(32);
+}
+}
+j++;
+}
+i++;
+}
+return (0);
+}
