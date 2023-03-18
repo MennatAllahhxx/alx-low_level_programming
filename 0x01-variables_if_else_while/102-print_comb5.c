b28281adfb79fbfe39fe9a50f33ec65b1b2f9ab3
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 19:10:23 -0700

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program to print possible combinations of 4 digits
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i = 0, j, k, l;
+while (i < 10)
+{
+while (j < 10)
+{
+k = i;
+while (k < 10)
+{
+l = j +1;
+while(l < 10)
+{
+if (! (i == k && j == l))
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(32);
+putchar('0' + k);
+putchar('0' + l);
+if ((i != 9) || (j != 8) || (k != 9) || (l != 9))
+{
+putchar(44);
+putchar(32);
+}
+}
+l++;
+}
+k++;
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
