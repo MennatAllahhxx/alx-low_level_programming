56cc36342e957beedb1ba6f6112c90accf2e3aeb
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-17 20:06:52 -0700

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,23 @@
  */
 int main(void)
 {
-int i = 0, c = 0, c2, j = 0, k = 0, l = 0;
-while (c <= 98)
+int p, q;
+for (p = 0; p <= 98; p++)
 {
-i = (c / 10 + '0');
-j = (c % 10 + '0');
-c2 = 0;
-while (c2 <= 99)
-{
-k = (c2 / 10 + '0');
-l = (c % 10 + '0');
 
-if (c < c2)
+for (q = p + 1; q<= 99; q++)
 {
-putchar('0' + i);
-putchar('0' + j);
+putchar('0' + (p / 10));
+putchar('0' + (p % 10));
 putchar(32);
-putchar('0' + k);
-putchar('0' + l);
-if (c != 98)
+putchar('0' + (q / 10));
+putchar('0' + (q % 10));
+if (p == 98 && q == 99)
 {
 putchar(44);
 putchar(32);
 }
 }
-c2++;
-}
-c++;
 }
 putchar('\n');
 return (0);
