0096c7c0cbc2899023d78822d4577ee190b1916f
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-21 14:20:44 -0700

    im fedral agent jack bauer and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * jack_bauer -  a function to print every minute
+ */
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (i = 0; i < 3; i++)
+{
+if (i < 2)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (l = 0; l < 10; l++)
+{
+_putchar('0' + i);
+_putchar('0' + j);
+_putchar(58);
+_putchar('0' + k);
+_putchar('0' + l);
+_putchar('\n');
+}
+}
+}
+}
+else
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (l = 0; l < 10; l++)
+{
+_putchar('0' + i);
+_putchar('0' + j);
+_putchar(58);
+_putchar('0' + k);
+_putchar('0' + l);
+_putchar('\n');
+}
+}
+}
+}
+}
+}
