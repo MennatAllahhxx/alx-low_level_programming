d1492bb3541e505a2830c89845e76aabb31acfe9
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-21 15:15:41 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function to add two numbers
+ * @i: first num
+ * @j: second num
+ * Return: addition of both numbers
+ */
+int add(int i, int j)
+{
+int s;
+s = i + j;
+return(s);
+}
