0ce2d865b205568fc25c1f7a5e69e136499704d4
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-21 15:07:38 -0700

    learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,11 @@ for (j = 0; j < 10; j++)
 {
 s = i * j;
 if (s < 10)
-{ 
+{
 if (j != 0)
-	_putchar(32);
+{
+_putchar(32);
+}
 _putchar(s + '0');
 }
 else
@@ -23,7 +25,6 @@ r = s % 10;
 _putchar(l + '0');
 _putchar(r + '0');
 }
-
 if (j < 9)
 {
 _putchar(44);
