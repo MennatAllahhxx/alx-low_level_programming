843c3e7466792722af3b8fd4d89431203f08b47a
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-21 18:24:56 -0700

    the world looks like multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,32 +6,32 @@
 void print_times_table(int n)
 {
 if (n < 16 && n > -1)
-{int i, j, s, l, m, r;
+{int i, j, s;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
-{
-s = i * j;
+{s = i * j;
 if (s < 10)
-{
-if (j != 0)
-{
-_putchar(32, 32);
+{ if (j != 0)
+{ _putchar(32);
+_putchar(32);
 }
 _putchar(s + '0');
 }
-else if (s > 9 && s < 100)
-{
-l = s / 10, r = s % 10;
-_putchar(32, l + '0', r + '0');
+else if (s > 9 && s < 100) {
+_putchar(32);
+_putchar((s / 10) + '0');
+_putchar((s % 10) + '0');
 }
 else
-{
-l = s / 100, m = (s % 100) / 10, r = s % 10;
-_putchar(l + '0', m + '0', r + '0');
+{ _putchar((s / 100) + '0');
+_putchar(((s % 100) / 10) + '0');
+_putchar((s % 10) + '0');
 }
 if (j < n)
-	_putchar(44, 32);
+{ _putchar(44);
+_putchar(32);
+}
 else
 	_putchar('\n');
 }
