b2dc85a92d975ce4a4c76a16fb5816e08f624f4e
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-21 17:21:38 -0700

    fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-unsigned int i = 0, c = 0, a = 1, b= 2;
-printf("%d, %d", a, b);
-while(i < 48)
+long c = 0, a = 1, b = 2;
+int i;
+printf("%ld, %ld", a, b);
+while (i < 48)
 {
 c = a + b;
-printf(", %d", c);
+printf(", %ld", c);
 a = b;
 b = c;
 i++;
