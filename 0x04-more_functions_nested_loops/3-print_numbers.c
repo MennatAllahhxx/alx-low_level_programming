7645e40d3f715f0ad4295b9bba67c3f68b0ab6a1
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-23 06:41:36 -0700

    the numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - fun to print numbers
+ * Return: nth
+ */
+void print_numbers(void)
+{
+int i;
+while(i <= 9)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
