45afdb18c73db32332ce63a72e9010f82c7f3033
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-23 04:38:47 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - a function to check for uppercase char
  * Return: 1 is uppercase char. 0 otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c <= 'Z' && c >= 'A')
 	return (1);
