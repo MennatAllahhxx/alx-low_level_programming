36f02623e444282590c156771c9a6d75763ef109
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-27 16:54:53 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - afun to copy string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
