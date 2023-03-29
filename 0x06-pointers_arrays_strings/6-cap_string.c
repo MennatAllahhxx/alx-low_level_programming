b6790be8b5383c85babc392ee6c4766b71f02f11
Mennat Allah Hassan <h.menna85@gmail.com>
2023-03-29 07:27:48 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *cap_string - fun to convert
+ * @s: string
+ * Return: result
+ */
+char *cap_string(char *s)
+{
+int i = 0, j;
+while (s[i] != '\0')
+{
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s [i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+{
+j = i + 1;
+if (s[j] >= 'a' && s[j] <= 'z')
+{
+s[j] = s[j] - 32;
+}
+}
+i++;
+}
+return (s);
+}
