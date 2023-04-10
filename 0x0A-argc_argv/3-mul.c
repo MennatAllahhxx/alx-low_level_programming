4d9930d839c0b8402719a1597a761c0193133b8c
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-10 14:54:12 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entery point
+ * @argc: operator
+ * @argv: operator
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+int i;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", i);
+return (0);
+}
