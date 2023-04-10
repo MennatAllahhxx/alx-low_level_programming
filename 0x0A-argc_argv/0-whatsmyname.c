47b7344b6c227db48a6be845a663917b1311f3b9
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-10 14:33:43 -0700

    it aint what they call you, it is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ * @argc: operator
+ * @argv: operator
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
