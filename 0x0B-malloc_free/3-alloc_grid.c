e0e2538731e6cba4b5ff73fe28527c39cc0f709a
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-11 11:42:06 -0700

    If you even dream of beating me youd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid -  a fun
+ * @width: w
+ * @height: h
+ * Return: NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **a;
+if (width <= 0 || height <= 0)
+	return (NULL);
+a = calloc(height, sizeof(int));
+if (a == NULL)
+	return (NULL);
+for (i = 0; i < height; i++)
+{
+a[i] = calloc(width, sizeof(int));
+if (a[i] ==  NULL)
+{
+for (j = 0; j < i; j++)
+	free(a[j]);
+free (a);
+return (NULL);
+}
+}
+return (a);
+}
