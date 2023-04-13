79f219f2d62ee5858596d1bbd4f5a44c5d67f62e
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-13 14:02:24 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - a fun
+ * @b: para
+ * Return: nth
+ */
+void *malloc_checked(unsigned int b)
+{
+void *a;
+a = malloc(b);
+if (a == NULL)
+	exit (98);
+return (a);
+}
