979dad9381c11d937e76dfce4474179267bf0aef
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-13 17:19:16 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_realloc - a fun
+ * @ptr: duhh
+ * @old_size: duhh
+ * @new_size: duhh
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+int *a;
+unsigned int i;
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (new_size == old_size)
+	return (ptr);
+if (ptr == NULL)
+{
+a = malloc(new_size);
+if (a == NULL)
+	return (NULL);
+return (a);
+}
+a = malloc(new_size);
+if (a == NULL)
+	return (NULL);
+for (i = 0; i < new_size && i < old_size; i++)
+	a[i] = ((int *)ptr)[i];
+free(ptr);
+return (a);
+}
