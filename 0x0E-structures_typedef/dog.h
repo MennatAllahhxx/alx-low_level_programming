08950cebcd88a1cf03081ef82ea2fe71b4fb09aa
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-17 12:45:11 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strcut dog - struct
+ * @name: 1st para
+ * @age: 2nd para
+ * @owner: 3rd para
+ */
+struct dog 
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
