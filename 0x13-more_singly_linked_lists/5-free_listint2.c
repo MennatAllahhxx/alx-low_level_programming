e6ca7da33f5a3de8d3ea6c0957803108ced6da91
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-29 02:09:11 -0700

    updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,14 @@
 void free_listint2(listint_t **head)
 {
 listint_t *n;
+if (!head)
+	return;
 while (*head)
 {
 n = *head;
 *head = (*head)->next;
 free(n);
 }
-free(n);
 free(*head);
 *head = NULL;
 }
