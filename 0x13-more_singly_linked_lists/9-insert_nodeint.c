48d543ec0fa1f662045bc8027ee80da25bd5ab3f
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-29 22:19:32 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * *insert_nodeint_at_index - a fun
+ * @head: op
+ * @idx: op
+ * @n: op
+ * Return: add
+ */
+listint_t *insert_nodeint_at_index(listint_t **head,
+		unsigned int idx, int n)
+{
+listint_t *new;
+unsigned int i;
+if (!head)
+	return (NULL);
+new = malloc(sizeof(listint_t));
+if (!new)
+	return (NULL);
+new->n = n;
+if (!idx)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+for (i = 0; *head && i < idx - 1; i++)
+{
+head = &(*head)->next;
+}
+if (!(*head && (*head)->next))
+{
+free(new);
+return (NULL);
+}
+new->next = (*head)->next;
+(*head)->next = new;
+return (new);
+}
