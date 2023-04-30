c7ccf0627439a16b0661bf3df8cb2e43ef8ccdf1
Mennat Allah Hassan <h.menna85@gmail.com>
2023-04-29 20:30:31 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - a fun
+ * @head: op
+ * Return: add
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+while (head->next)
+{
+sum += head->n;
+head = head->next;
+}
+if (sum)
+	sum += head->n;
+return (sum);
+}
