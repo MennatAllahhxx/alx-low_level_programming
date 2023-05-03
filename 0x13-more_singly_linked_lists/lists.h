fca11896564bdedda35b3623cefa73ca807af71d
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-02 20:00:51 -0700

    updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,12 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+typedef struct listint_safe
+{
+const listint_t *addy;
+struct listint_safe *next;
+} listint_safe;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
