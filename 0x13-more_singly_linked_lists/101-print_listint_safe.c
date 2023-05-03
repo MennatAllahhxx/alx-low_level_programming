bbd24925a1a7a011118d843c2467ccb987efcf3b
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-02 20:08:51 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,41 @@
 #include "lists.h"
 /**
+ * _check_and_print - Check the list and print
+ * @head: of newlist to check
+ * @prev: node on list to check
+ *
+ * Return: number of nodes
+ */
+int _check_and_print(const listint_t *head, listint_safe *prev)
+{
+listint_safe n, *temp;
+if (head->next == NULL)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+return (1);
+}
+n.next = prev;
+n.addy = head;
+temp = n.next;
+while (temp != NULL && temp->addy != head)
+	temp = temp->next;
+if (temp != NULL)
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+return (0);
+}
+printf("[%p] %d\n", (void *)head, head->n);
+return (1 + _check_and_print(head->next, &n));
+}
+
+/**
  * print_listint_safe - a fun
  * @head: op
  * Return: sth
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t size = 0;
-const listint_t *temp = NULL;
 if (!head)
 	exit(98);
-while (head)
-{
-size++;
-printf("[%p] %d\n", (void *)head, head->n);
-if (head < head->next && head->next)
-{
-temp = head;
-head = head->next;
-}
-else
-{
-head = head->next;
-printf("-> [%p] %d\n", (void *)temp, temp->n);
-break;
-}
-}
-return (size);
+return (_check_and_print(head, NULL));
 }
