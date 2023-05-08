0ab4adcd4f234a98c202fde810865b00ce32b16f
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 07:09:17 -0700

    updated

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - a fun
+ * @filename: op
+ * @letters: op
+ * Return: sth
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char buf[1024];
+int fd;
+int byrd, bywr;
+if (!filename)
+	return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+	return (0);
+byrd = read(fd, buf, letters);
+if (byrd == -1)
+	return (0);
+bywr = write(STDOUT_FILENO, buf, byrd);
+if (bywr == -1 || bywr != byrd)
+	return (0);
+close(fd);
+return (byrd);
+}
