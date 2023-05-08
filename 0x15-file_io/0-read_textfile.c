0edf4c13b80e4e6dbf8c11f09a2bf3e7fe34e723
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 07:37:08 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,13 +9,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 char buf[1024];
 int fd;
-int byrd, bywr;
+int byrd = 0, bywr;
 if (!filename)
 	return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 	return (0);
 byrd = read(fd, buf, letters);
+if (byrd < (int)letters)
+	byrd += read(fd, buf, letters);
 if (byrd == -1)
 	return (0);
 bywr = write(STDOUT_FILENO, buf, byrd);
