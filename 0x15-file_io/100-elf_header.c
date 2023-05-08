b135a872e90cc5e4085ccfc62d42e54c69565854
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 16:32:16 -0700

    updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -36,13 +36,22 @@ printf("Magic:   ");
 for (i = 0; i < EI_NIDENT; i++)
 	printf("%02x ", elf_header.e_ident[i]);
 printf("\n");
-printf("Class:                             %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-printf("Data:                              %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-printf("Version:                           %d\n", elf_header.e_ident[EI_VERSION]);
-printf("OS/ABI:                            %d\n", elf_header.e_ident[EI_OSABI]);
-printf("ABI Version:                       %d\n", elf_header.e_ident[EI_ABIVERSION]);
-printf("Type:                              %d\n", elf_header.e_type);
-printf("Entry point address:               %lx\n", elf_header.e_entry);
+printf("Class:                             %s\n",
+		elf_header.e_ident[EI_CLASS] == ELFCLASS64 ?
+		"ELF64" : "ELF32");
+printf("Data:                              %s\n",
+		elf_header.e_ident[EI_DATA] == ELFDATA2LSB ?
+		"2's complement, little endian" : "2's complement, big endian");
+printf("Version:                           %d\n",
+		elf_header.e_ident[EI_VERSION]);
+printf("OS/ABI:                            %d\n",
+		elf_header.e_ident[EI_OSABI]);
+printf("ABI Version:                       %d\n",
+		elf_header.e_ident[EI_ABIVERSION]);
+printf("Type:                              %d\n",
+		elf_header.e_type);
+printf("Entry point address:               %lx\n",
+		elf_header.e_entry);
 if (close(fd) == -1)
 	print_error("Error: could not close ELF file");
 return (0);
