c0770a394681ac9bd2c9d77964b969490253782a
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 12:15:12 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,21 +16,26 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 fd1 = open(argv[1], O_RDONLY);
-while (!EOF)
-{
 byrd = read(fd1, buf, 1024);
 if (fd1 == -1 || byrd == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-}
 fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd2 == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+exit(99);
+}
+while (byrd > 0)
+{
 bywr = write(fd2, buf, byrd);
 if (fd2 == -1 || bywr  == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 exit(99);
+}
 }
 if (close(fd1) || close(fd2) == -1)
 {
