fc992beaa6b87819ee3edb0428ea31272d9a6357
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 09:34:42 -0700

    updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_file - a fun
+ * @filename: op
+ * @text_content: op
+ * Return: sth
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int bywr;
+if (!filename)
+	return (-1);
+fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+if (fd == -1)
+	return (-1);
+bywr = write(fd, text_content, sizeof(text_content) - 2);
+if (bywr == -1)
+	return (-1);
+return (1);
+}
