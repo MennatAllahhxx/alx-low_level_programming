e1c50b86ec4674e54d62a0aad6d22127ce7fa094
Mennat Allah Hassan <h.menna85@gmail.com>
2023-05-08 22:17:59 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,35 +2,25 @@
 /**
  * err - a fun
  * @errtype: op
+ * @fd1: op
+ * @fd2: op
+ * @file1: op
+ * @file2: op
  * Return: sth
  */
-int err(int errtype)
+int err(int errtype, char *file)
 {
-close(fd1);
-close(fd2);
-if (errtype == 97)
+if (errtype == 98)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-else if (errtype == 98)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 exit(98);
-}
-else if (errtype == 99)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
 }
 else
 {
-if (close(fd1) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-if (close(fd2) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+exit(99);
 }
+return (0);
 }
 
 /**
@@ -44,27 +34,39 @@ int main(int argc, char **argv)
 char buf[1024];
 int fd1, fd2;
 int byrd, bywr;
+int x = 0, y = 0;
 if (argc != 3)
-	err(97);
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 fd1 = open(argv[1], O_RDONLY);
 if (fd1 == -1)
-	err(98);
+	err(98, argv[1]);
 byrd = read(fd1, buf, 1024);
 if (byrd == -1)
-	err(98);
+	err(98, argv[1]);
 fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd2 == -1)
-	err(99);
+	err(99, argv[2]);
 while (byrd > 0)
 {
 bywr = write(fd2, buf, byrd);
 if (bywr  == -1 || byrd != bywr)
-	err(99);
+	err(99, argv[2]);
 byrd = read(fd1, buf, 1024);
 if (byrd == -1)
-	err(98);
+	err(98, argv[1]);
 }
-if (close(fd1) || close(fd2) == -1)
-	err(100);
+x = close(fd1);
+y = close(fd2);
+if (x == -1 || y == -1)
+{
+if (x == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+if (y == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+exit(100);
+}
 return (0);
 }
