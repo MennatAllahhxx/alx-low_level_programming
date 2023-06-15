5c6e6a257a2ed1d522d6d04ce0babd4e73d6b481
Menna Hassan <h.menna85@gmail.com>
2023-06-15 16:13:31 -0700

    updated

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * free_dnodeint - a fun to free list
+ * free_dlistint - a fun to free list
  * @head: head
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *node;
-	
+
 	while (head)
 	{
 		node = head;
