e4dba154dd827eaef510c6b1fae52b0bcc590907
Menna Hassan <h.menna85@gmail.com>
2023-06-15 19:20:57 -0700

    updated

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - a fun to delete
+ * @head: head
+ * @index: index
+ * Return: 1 if success
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int i;
+
+	if (!head || !(*head))
+		return (-1);
+	node = *head;
+	if (!index && node->next)
+	{
+		node->next->prev = NULL;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	for (i = 0; i < index && node; i++)
+		node = node->next;
+	if (node->next && node->prev)
+	{
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		free(node);
+		return (1);
+	}
+	else if (node->prev && !node->next)
+	{
+		node->prev->next = NULL;
+		free(node);
+		return (1);
+	}
+	return (-1);
+}
