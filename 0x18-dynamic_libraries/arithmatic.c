3dc717d84f98d940eb3cfb6ebd24a10b02e87240
Menna Hassan <h.menna85@gmail.com>
2023-06-19 15:19:59 -0700

    updated

---
+++ 0x18-dynamic_libraries/arithmatic.c
@@ -0,0 +1,69 @@
+#include "py.h"
+/**
+ * add - fun to add
+ * @a: 1st digit
+ * @b: 2nd digit
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	return (result);
+}
+
+
+/**
+ * sub - fun to sub
+ * @a: 1st digit
+ * @b: 2nd digit
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	int result;
+	result = a - b;
+	return (result);
+}
+
+
+/**
+ * mul - fun to mul
+ * @a: 1st digit
+ * @b: 2nd digit
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+	return (result);
+}
+
+
+/**
+ * div - fun to div
+ * @a: 1st digit
+ * @b: 2nd digit
+ * Return: result
+ */
+int div(int a, int b)
+{
+	int result;
+	result = a / b;
+	return (result);
+}
+
+
+/**
+ * mod - fun to mod
+ * @a: 1st digit
+ * @b: 2nd digit
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	int result;
+	result = a % b;
+	return (result);
+}
