2b0d45c779196b817ff8f08927230e0b00c89714
Menna Hassan <h.menna85@gmail.com>
2023-07-07 16:46:58 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,42 +7,44 @@
  * @value: value of hash item
  * Return: 1 if success
  */
-int hash_table_set(hash_table_t *ht, const char *key,
-		const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *item, *curr;
 	unsigned long int index = 0;
 
 	item = (hash_node_t *)malloc(sizeof(hash_node_t));
-
 	if (!item || !ht || !key || !(*key))
 		return (0);
-
 	strcpy(item->key, key);
 	strcpy(item->value, value);
 	index = key_index((unsigned char *) key, ht->size);
-
 	if (ht->array[index])
 	{
 		curr =  ht->array[index];
-
 		while (curr != NULL)
 		{
 			if (!strcmp(curr->key, key))
-			{
-				strcpy(curr->key, key);
-				free(item);
-				return (1);
-			}
+				break;
 			curr = curr->next;
 		}
-		item->next = ht->array[index];
+		if (!curr)
+		{
+			item->next = ht->array[index];
+			ht->array[index] = item;
+		}
+		else
+		{
+			free(curr->value);
+			strcpy(curr->value, value);
+			free(item->value);
+			free(item->key);
+			free(item);
+		}
 	}
-
 	else
+	{
 		item->next = NULL;
-
-	ht->array[index] = item;
-
+		ht->array[index] = item;
+	}
 	return (1);
 }
