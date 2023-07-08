564d5652a9fd2d6bc8dd3aa18515fffe5b83a793
Menna Hassan <h.menna85@gmail.com>
2023-07-07 18:10:47 -0700

    updated

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - fun to delete hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht)
+		return;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+
+		while (ht->array[index])
+		{
+			node = node->next;
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+			ht->array[index] = node;
+		}
+		free(ht->array[index]);
+	}
+	free(ht->array);
+	free(ht);
+}
