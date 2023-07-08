f2241e68fbc1f6fa5cbc6313795819f5d0c53c33
Menna Hassan <h.menna85@gmail.com>
2023-07-07 17:50:26 -0700

    updated

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - fun to print hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	char *separator;
+
+	printf("{");
+	separator = "";
+
+	for (index = 0; index < ht->size; index++)
+	{
+		while (ht->array[index])
+		{
+			printf("%s'%s': '%s'", separator,
+					ht->array[index]->key,
+					ht->array[index]->value);
+			ht->array[index] = ht->array[index]->next;
+			separator = ", ";
+		}
+	}
+
+	printf("}\n");
+}
