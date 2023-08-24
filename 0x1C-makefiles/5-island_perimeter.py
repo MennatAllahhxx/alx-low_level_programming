d49472159a37a4bd2768278b27a8e105ead4190f
Menna Hassan <h.menna85@gmail.com>
2023-08-23 19:53:54 -0700

    updated

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This is module '5-island_perimeter'
+
+This module contains one function: island_perimeter
+"""
+
+
+def island_perimeter(grid):
+        """
+            an instance to find per of island
+                :param grid: grid to look inside
+                    :return: perimeter of the island
+                        """
+                            d = 0
+                                perimeter = 0
+                                    height = len(grid)
+                                        length = len(grid[0])
+                                            for i in grid:
+                                                        c = 0
+                                                                for j in i:
+                                                                                if j == 1:
+                                                                                                    surr = 4
+                                                                                                                    if c != (length - 1):
+                                                                                                                                            if grid[d][c + 1] == 1:
+                                                                                                                                                                        surr -=1
+                                                                                                                                                                                        if c != 0:
+                                                                                                                                                                                                                if grid[d][c - 1] == 1:
+                                                                                                                                                                                                                                            surr -= 1
+                                                                                                                                                                                                                                                            if d != (height -1):
+                                                                                                                                                                                                                                                                                    if grid[d + 1][c] == 1:
+                                                                                                                                                                                                                                                                                                                surr -= 1
+                                                                                                                                                                                                                                                                                                                                if d != 0:
+                                                                                                                                                                                                                                                                                                                                                        if grid[d - 1][c] == 1:
+                                                                                                                                                                                                                                                                                                                                                                                    surr -= 1
+                                                                                                                                                                                                                                                                                                                                                                                                    perimeter +=surr
+                                                                                                                                                                                                                                                                                                                                                                                                                c += 1
+                                                                                                                                                                                                                                                                                                                                                                                                                        d += 1
+                                                                                                                                                                                                                                                                                                                                                                                                                            return (perimeter)
+
